9b651e08f07226b879556448a45d65804d858157
James Rawlings <rawlingsj80@gmail.com>
2021-05-11 14:15:59 +0100

    feat: lets make the filter configurable and allow a dry run option

--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.15
 require (
 	cloud.google.com/go v0.79.0
 	github.com/golang/protobuf v1.5.1 // indirect
-	github.com/jenkins-x/jx-logging/v3 v3.0.3 // indirect
+	github.com/jenkins-x/jx-logging/v3 v3.0.3
+	github.com/lunixbochs/vtclean v1.0.0 // indirect
+	github.com/manifoldco/promptui v0.8.0
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
-	golang.org/x/sys v0.0.0-20210317225723-c4fcb01b228e // indirect
+	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
 	google.golang.org/api v0.42.0
 	google.golang.org/genproto v0.0.0-20210317182105-75c7a8546eb9
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jenkins-x/jx-logging/v3/pkg/log"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"github.com/manifoldco/promptui"
 	"google.golang.org/api/iterator"
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
@@ -16,11 +17,15 @@ import (
 var (
 	options struct {
 		projectID string
+		filter    string
+		dryRun    bool
 	}
 )
 
 func init() {
 	flag.StringVar(&options.projectID, "project", "", "GCP project with which to communicate")
+	flag.StringVar(&options.filter, "filter", "pr-", "match secret names that contain this string, defaults to 'pr-'")
+	flag.BoolVar(&options.dryRun, "dry-run", false, "dry run will print which secrets would be deleted but does not perform the actual delete action")
 
 }
 
@@ -29,6 +34,16 @@ func main() {
 
 	if options.projectID == "" {
 		log.Logger().Fatal("missing --project flag")
+	}
+
+	if options.filter == "" {
+		log.Logger().Warnf("You are about to delete ALL secrets, from GCP project '%s'", options.projectID)
+	} else {
+		log.Logger().Warnf("You are about to delete secrets that contain '%s' in the name, from GCP project '%s'", options.filter, options.projectID)
+	}
+
+	if !yesNo() {
+		return
 	}
 
 	// Create the client.
@@ -58,22 +73,42 @@ func main() {
 			log.Logger().Fatalf("failed to list secret versions: %v", err)
 		}
 
-		if !strings.Contains(resp.Name, "pr-") {
+		if options.filter != "" && !strings.Contains(resp.Name, options.filter) {
 			// lets only GC secrets for version stream pull requests
 			continue
 		}
 		count++
-		// Build the request.
-		req := &secretmanagerpb.DeleteSecretRequest{
-			Name: resp.Name,
+
+		if options.dryRun {
+			log.Logger().Infof("dry run: found secret to delete: %s", resp.Name)
+			continue
+
+		} else {
+			// Build the request.
+			req := &secretmanagerpb.DeleteSecretRequest{
+				Name: resp.Name,
+			}
+
+			// Call the API.
+			if err := client.DeleteSecret(ctx, req); err != nil {
+				log.Logger().Fatalf("failed to delete secret: %v", err)
+			}
+
+			log.Logger().Infof("deleted secret %s\n", resp.Name)
 		}
 
-		// Call the API.
-		if err := client.DeleteSecret(ctx, req); err != nil {
-			log.Logger().Fatalf("failed to delete secret: %v", err)
-		}
-
-		log.Logger().Infof("deleted secret %s\n", resp.Name)
 	}
 	log.Logger().Infof("deleted %v secrets", count)
+}
+
+func yesNo() bool {
+	prompt := promptui.Select{
+		Label: "Are you sure [Yes/No]",
+		Items: []string{"Yes", "No"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Logger().Fatal("Prompt failed %v", err)
+	}
+	return result == "Yes"
 }
